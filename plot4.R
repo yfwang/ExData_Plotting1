fd20c70373693a2fd2678350f081bed4e151f604
yfwang <goodyufu@gmail.com>
2017-07-01 11:26:38 +0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -15,8 +15,9 @@ png(filename='plot4.png', width=480, height=480, units='px')
 par(mfrow=c(2,2))
 linecolors <- c("black","red","blue")
 linelabels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-plot(finalData$SetTime, finalData$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
-plot(finalData$SetTime, finalData$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power")
+
+plot(finalData$SetTime, finalData$Voltage, type="l", col="black", xlab="datetime", ylab="Voltage")
 
 
 plot(finalData$SetTime, finalData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
@@ -25,7 +26,7 @@ lines(finalData$SetTime, finalData$Sub_metering_3, type="l", col="blue")
 legend("topright", bty="n", legend=linelabels, lty=1, col=linecolors)
 
 
-plot(finalData$SetTime, finalData$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
+plot(finalData$SetTime, finalData$Global_reactive_power, type="l", col="black", xlab="datetime", ylab="Global_reactive_power")
 
 
 dev.off()
